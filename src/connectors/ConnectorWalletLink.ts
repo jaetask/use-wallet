434c3f27cae0d5489da8bbbdd5be3076d4a9a0c8
jaetask <me@jaetask.com>
2022-02-09 12:54:35 +0000

    remove chain id limitation

--- src/connectors/ConnectorWalletLink.ts
+++ src/connectors/ConnectorWalletLink.ts
@@ -8,21 +8,14 @@ export default async function init(): Promise<Connector> {
   )
   return {
     web3ReactConnector({
-      chainId,
       url,
       appName,
       appLogoUrl,
     }: {
-      chainId: number
       url: string
       appName: string
       appLogoUrl: string
     }) {
-      if (chainId !== 1) {
-        throw new ConnectorConfigError(
-          'The WalletLink connector requires chainId to be 1.'
-        )
-      }
       if (!/^https?:\/\//.test(url)) {
         throw new ConnectorConfigError(
           'The WalletLink connector requires url to be an HTTP URL.'
